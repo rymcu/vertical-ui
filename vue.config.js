6c05fa9d3e626b1ffb8f32fdfd812ebea8fc9896
ronger <ronger-x@outlook.com>
2019-12-06 17:56:28 +0800

    添加分析工具

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,15 @@
 module.exports = {
+    chainWebpack: (config)=>{
+        /* 添加分析工具*/
+        if (process.env.NODE_ENV === 'production') {
+            if ( process.env.npm_config_report ) {
+                config.plugin('webpack-bundle-analyzer')
+                    .use( require( 'webpack-bundle-analyzer' ).BundleAnalyzerPlugin)
+                    .end();
+                config.plugins.delete( 'prefetch' )
+            }
+        }
+    },
     devServer: {
         port: 8081,
         proxy: {
