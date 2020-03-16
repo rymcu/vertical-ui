ff6212994c81ce67a52401634e79582aa7930811
ronger <ronger-x@outlook.com>
2020-03-16 11:34:02 +0800

    :children_crossing: 页脚手机端显示优化

--- src/components/classic/Footer.vue
+++ src/components/classic/Footer.vue
@@ -1,13 +1,15 @@
 <template>
     <el-row class="footer">
         <el-col style="text-align: center;">
-            <el-col :xs="24" :xl="12">
+            <el-col :xs="24" :sm="24" :xl="12">
                 <span>{{ slogan }}</span>
+                <span>{{ slogan_en }}</span>
             </el-col>
-            <el-col :xs="24" :xl="12">
+            <el-col :xs="24" :sm="24" :xl="12">
                 <el-col class="row align-items-center">
                     <el-col class="col-auto">
-                        Copyright © 2020 <el-link :underline="false" href="/"><span>{{ systemName }}</span></el-link>.  <el-link :underline="false" href="http://www.beian.miit.gov.cn/"><span style="margin-left: 1.5rem;">{{ beiAn }}</span></el-link>
+                        Copyright © 2020 <el-link :underline="false" href="/" style="vertical-align: baseline;"><span>{{ systemName }}</span></el-link>.
+                        <el-link :underline="false" href="http://www.beian.miit.gov.cn/" style="vertical-align: baseline;"><span>{{ beiAn }}</span></el-link>
                     </el-col>
                 </el-col>
             </el-col>
@@ -23,7 +25,8 @@
                 systemTitle: '\u7f57\u5409\u7f51\u0020\u002d\u0020\u5185\u5bb9\u5206\u4eab\u751f\u6001\u5e73\u53f0',
                 systemName: 'RYMCU',
                 systemUrl: 'https://rymcu.com',
-                slogan: 'rymcu · 嵌入式知识共享平台 Embedded knowledge sharing platform',
+                slogan: 'rymcu · 嵌入式知识共享平台 ',
+                slogan_en: 'Embedded knowledge sharing platform',
                 beiAn: '沪ICP备19042611号'
             }
         }
